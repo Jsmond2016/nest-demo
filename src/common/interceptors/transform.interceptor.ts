44825db3acd57fdeda5f0cec46731162a7c39a9b
Jsmond2016 <Jsmond2016@gmail.com>
2021-11-18 11:21:58 +0800

    响应体格式优化

---
+++ src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const { statusCode } = context.getArgByIndex(0).res;
+    console.log('args: ', statusCode);
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          data,
+          code: 0,
+          msg: '请求成功',
+        };
+      }),
+    );
+  }
+}
